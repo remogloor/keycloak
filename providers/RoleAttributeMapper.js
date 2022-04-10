e026e59aa790f09f5430cb9a274f8b750e98d171
Remo Gloor <remo.gloor@gmail.com>
2022-04-10 05:33:48 +0200

    Update RoleAttributeMapper.js

--- providers/RoleAttributeMapper.js
+++ providers/RoleAttributeMapper.js
@@ -1,6 +1,8 @@
 var attributes = {};
 
-user.getRoleMappings().forEach(function(roleModel) {
+//user.getRoleMappings().forEach(function(roleModel) {
+var client = keycloakSession.getContext().getClient();
+user.getClientRoleMappings(client).forEach(function(roleModel) {    
     var map = roleModel.getAttributes();
     map.forEach(function(key, value){
         attributes[key] = value;
