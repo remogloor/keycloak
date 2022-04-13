f5434279ebf8ab87c1cf252c6ab9136c3baa84da
Remo Gloor <remo.gloor@gmail.com>
2022-04-13 19:12:03 +0200

    Add composite roles

--- providers/RoleAttributeMapper.js
+++ providers/RoleAttributeMapper.js
@@ -1,5 +1,20 @@
 var attributes = {};
 
+function processRole(roleModel, attr) {
+    var map = roleModel.getAttributes();
+    map.forEach(function(key, value){
+        attr[key] = value;
+    });
+    
+    if (roleModel.isComposite()) {
+        roleModel.getComposites().forEach(function(roleModel) {
+            attr = processRole(roleModel, attr);
+        });
+    }
+    
+    return attr;
+}
+
 user.getGroups().forEach(function(groupModel) {
     var map = groupModel.getAttributes();
     map.forEach(function(key, value){
@@ -7,18 +22,12 @@ user.getGroups().forEach(function(groupModel) {
     });
     
     groupModel.getRoleMappings().forEach(function(roleModel) {
-        var map = roleModel.getAttributes();
-        map.forEach(function(key, value){
-            attributes[key] = value;
-        }); 
+        attributes = processRole(roleModel, attributes);
     });
 });
 
 user.getRoleMappings().forEach(function(roleModel) {
-    var map = roleModel.getAttributes();
-    map.forEach(function(key, value){
-        attributes[key] = value;
-    }); 
+    attributes = processRole(roleModel, attributes);
 });
 
 exports = attributes;
