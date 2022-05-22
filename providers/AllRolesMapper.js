56c5a33a044a732633fc192aadcb60c517110cfd
Remo Gloor <remo.gloor@gmail.com>
2022-05-22 14:22:01 +0200

    Process groups and composite roles
    Process groups and composite roles to get all role names

--- providers/AllRolesMapper.js
+++ providers/AllRolesMapper.js
@@ -1,7 +1,26 @@
-var roles = [];
+var roles = new Set();
+
+function processRole(roleModel, roleSet) {
+    var name = roleModel.getName();
+    if (name.startsWith("HW_") roleSet.add(name);
+    
+    if (roleModel.isComposite()) {
+        roleModel.getComposites().forEach(function(roleModel) {
+            roleSet = processRole(roleModel, roleSet);
+        });
+    }
+    
+    return roleSet;
+}
+
+user.getGroups().forEach(function(groupModel) {    
+    groupModel.getRoleMappings().forEach(function(roleModel) {
+        roles = processRole(roleModel, roles);
+    });
+});
 
 user.getRoleMappings().forEach(function(roleModel) {
-    roles.push(roleModel.getName());
+    roles = processRole(roleModel, roles);
 });
 
 exports = roles;
